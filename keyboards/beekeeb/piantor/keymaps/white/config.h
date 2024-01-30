cbe754778ccd37763616700891de863f5b742f2a
Michael White <michael.p.m.white@gmail.com>
2024-01-30 14:42:29 -0700

    Fix left/right microcontroller detection

--- keyboards/beekeeb/piantor/keymaps/white/config.h
+++ keyboards/beekeeb/piantor/keymaps/white/config.h
@@ -6,7 +6,13 @@
 // It appears that the Waveshare RP2040-plus (which I am using) lacks a VBUS pin.
 // At the very least, the keyboard currently behaves very differently depending
 // on which half I plug in when USB_VBUS_PIN is set.
-#undef USB_VBUS_PIN
+// If we define EE_HANDS and include an extra flag when flashing, we can include
+// extra information in EEPROM which tells each half which side it's on.
+// Note that the qmk command in documentation didn't work for me; I had use the
+// following make command:
+// make beekeeb/piantor:white:uf2-split-left
+// for the left, and the same command with "uf2-split-right" for the right half.
+#define EE_HANDS
 
 #define TAPPING_TERM  200
 #define PERMISSIVE_HOLD
